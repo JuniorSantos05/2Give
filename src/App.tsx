de085c426dc2d3e8585cea9452df2f9b926fe38c
hermlandim <hermerson_landim@hotmail.com>
2022-11-09 10:26:36 -0300

    Refatora registerPage 1.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Global from "./styles/Global/global";
 import RoutesMain from "./routes";
 import { ToastContainer } from "react-toastify";
 import GiveProvider from "./contexts/GiveContext";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,25 +1,40 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useContext, useState } from "react";
 import { FieldError, UseFormRegister } from "react-hook-form";
+import { GiveContext } from "../../contexts/GiveContext";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error: FieldError | undefined;
   register: UseFormRegister<any>;
-  id: string;
+  name: string;
 }
 
 export const Input = ({
   label,
   error,
   register,
-  id,
-  placeholder,
+  name,
   ...rest
 }: IInputProps) => {
   return (
     <>
       <label>{label}</label>
-      <input {...rest} {...register(id)} placeholder={placeholder} />
+      <input {...rest} {...register(name)} />
+      {error && <span className="error">{error?.message}</span>}
+    </>
+  );
+};
+
+interface IInputPropsNormal extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  error: FieldError | undefined;
+}
+
+export const InputNormal = ({ label, error, ...rest }: IInputPropsNormal) => {
+  return (
+    <>
+      <label>{label}</label>
+      <input {...rest} />
       {error && <span className="error">{error?.message}</span>}
     </>
   );
